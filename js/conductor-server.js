eb57eceb503a14a6e938340f9cfca405b89ef9b9
kenchan0130 <t-detective.onishi46@i.softbank.jp>
2014-03-08 16:50:55 +0900

    Edit .gitignore file.

---
+++ js/conductor-server.js
@@ -0,0 +1,29 @@
+var apiDomain = 'http://localhost:9393';
+var request   = require('request');
+var async     = require('async');
+var ConductorServer = {};
+
+ConductorServer.prototype.save = function (word, object, img) {
+    async.series([
+        function (callback) {
+            var targetURL = apiDomain + '/save';
+            var options = {
+                uri: targetURL,
+                form: { word: word, object: object, img: img },
+                json: true
+            };
+            request.post(options, function(error, response, body){
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                } else {
+                    console.log('error: '+ response.statusCode);
+                }
+            });
+        }
+    ], function (err, results) {
+        if (err) {
+            throw err;
+        }
+        console.log('series all done. ' + results);
+    });
+};
