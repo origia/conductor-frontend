dea65f1995f8d03c402b49858ef2bc6e1bf98f7b
kenchan0130 <t-detective.onishi46@i.softbank.jp>
2014-03-09 06:46:20 +0900

    Separated api functions.

--- js/conductor-server.js
+++ js/conductor-server.js
@@ -4,24 +4,14 @@ var async     = require('async');
 var ConductorServer = function () {};
 
 
-/*
-@param {word: word, img: img, object: object}
-@func  callback function
-
-ex.
-var cs = new ConductorServer();
-cs.save({word: word, img: img, object: object}, function(results){
-    console.log(results);
-})
-*/
-ConductorServer.prototype.save = function (param, func) {
+ConductorServer.prototype._post = function (apiPath, param, func) {
     if (param === null || param === void 0) {
         console.log("error: param is nil.");
         return false;
     }
     async.series([
         function (callback) {
-            var targetURL = apiDomain + '/save';
+            var targetURL = apiDomain + apiPath;
             var options = {
                 uri: targetURL,
                 form: param,
@@ -41,6 +31,89 @@ ConductorServer.prototype.save = function (param, func) {
             throw err;
         }
         if (func) func(results);
-        console.log('series all done. ', results);
     });
+};
+ConductorServer.prototype._get = function (apiPath, func) {
+    async.series([
+        function (callback) {
+            var targetURL = apiDomain + apiPath;
+            var options = {
+                uri: targetURL,
+                //form: param,
+                json: true
+            };
+            request.get(options, function(error, response, body){
+                if (!error && response.statusCode === 200) {
+                    callback(null, body);
+                } else {
+                    console.log('error: '+ response.statusCode);
+                    callback(response.statusCode);
+                }
+            });
+        }
+    ], function (err, results) {
+        if (err) {
+            throw err;
+        }
+        if (func) func(results);
+    });
+};
+
+
+/*
+@param {word: word, img: img, object: object, currentTime: currentTime}
+@func  callback function
+
+ex.
+var cs = new ConductorServer();
+cs.searchSave({word: word, img: img, object: object, currentTime: currentTime}, function(results){
+    console.log(results);
+})
+*/
+ConductorServer.prototype.searchSave = function(param, func){
+    var apiPath = '/search/save';
+    this._post(apiPath, param, func);
+};
+
+/*
+@func  callback function
+
+ex.
+var cs = new ConductorServer();
+cs.searchHistory(function(results){
+    console.log(results);
+})
+*/
+ConductorServer.prototype.searchHistory = function(func){
+    var apiPath = '/search/history';
+    this._get(apiPath, func);
+};
+
+/*
+@param {word: word, currentTime: currentTime}
+@func  callback function
+
+ex.
+var cs = new ConductorServer();
+cs.wishSave({word: word, currentTime: currentTime}, function(results){
+    console.log(results);
+})
+*/
+ConductorServer.prototype.wishSave = function(param, func){
+    var apiPath = 'wish/save';
+    this._post(apiPath, param, func);
+};
+
+/*
+@func  callback function
+
+ex.
+var cs = new ConductorServer();
+cs.wishList(function(results){
+    console.log(results);
+})
+*/
+ConductorServer.prototype.wishList = function(func){
+    var apiPath = '/wish/list';
+    this._get(apiPath, func);
 };
