60716756de4d162d72d53f3490773f1bbbf84782
yoko <yoko.ichioka@is.ocha.ac.jp>
2014-03-09 03:20:27 +0900

    ポインタと線

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 window.App = window.App || {};
 
-var LeapManager = require('leap-events').LeapManager;
+//var LeapManager = require('leap-events').LeapManager;
+var LeapManager = require('leap-events').LeapManager
+, FrameState  = require('leap-events').FrameState
+FrameState.screenSize = { width: 1280, height: 1080}
+FrameState.leapFrameSize ={ width: 350, height: 300}
+
 
 App.leapManager = new LeapManager({
 });
